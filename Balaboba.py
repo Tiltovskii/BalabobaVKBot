336badb0b429d63767b72e154654772e44a10cec
Tiltovskii <benderabenderabendera@gmail.com>
2022-01-20 16:50:26 +0300

    Changed the reading balaboba`s response using regular strings to a json file

--- Balaboba.py
+++ Balaboba.py
@@ -1,6 +1,5 @@
 import json
 import urllib.request
-import re
 from fake_useragent import UserAgent
 
 
@@ -12,36 +11,14 @@ async def zabalobobit(query, intro=1):
         'Origin': 'https://yandex.ru',
         'Referer': 'https://yandex.ru/',
     }
-
     API_URL = 'https://zeapi.yandex.net/lab/api/yalm/text3'
     payload = {"query": query, "intro": intro, "filter": 1}
     params = json.dumps(payload).encode('utf8')
     req = urllib.request.Request(API_URL, data=params, headers=headers)
     response = urllib.request.urlopen(req)
-    text = response.read().decode('unicode-escape')
-    groups = re.findall(r"(?<=[\"':])[^\"']+", text)
-    text = groups[10]
-    return query + ' ' + text
+    data = response.read()
+    encoding = response.info().get_content_charset('utf-8')
+    json_file = json.loads(data.decode(encoding))
+    return query + ' ' + json_file['text']
 
 
-if __name__ == '__main__':
-    user_agent = UserAgent().chrome
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': user_agent,
-        'Origin': 'https://yandex.ru',
-        'Referer': 'https://yandex.ru/',
-    }
-
-    API_URL = 'https://zeapi.yandex.net/lab/api/yalm/text3'
-    payload = {"query": "Скорпионы", "intro": 1, "filter": 1}
-    params = json.dumps(payload).encode('utf8')
-    req = urllib.request.Request(API_URL, data=params, headers=headers)
-    response = urllib.request.urlopen(req)
-    text = response.read().decode('unicode-escape')
-    print(text)
-    groups = re.findall(r"(?<=[\"':])[^\"']+", text)
-    query = groups[6]
-    text = groups[10]
-    print(query + text)
-    # print(zabalobobit('привет всем'))
